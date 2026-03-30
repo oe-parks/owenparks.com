d1b34c1f5e949b638f3304ff3bc85ba44147c764
oe-parks <owenp1@uw.edu>
2026-03-30 18:10:30 +0000

    Deploying to gh-pages from @ oe-parks/owenparks.com@fd552405f3b4756ca4be9fc38067798b72d98f4f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -446,14 +446,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
         },
       },{
         id: 'social-rss',
