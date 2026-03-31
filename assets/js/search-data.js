f9f3d91f4688083012cdf7502888b99278ef46f0
oe-parks <owenp1@uw.edu>
2026-03-31 07:09:27 +0000

    Deploying to gh-pages from @ oe-parks/owenparks.com@eee2cd893b35ac11c2b24b6f598b6244f14674e8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,13 +448,6 @@ ninja.data = [{
           window.open("https://open.spotify.com/user/o.parks11", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
